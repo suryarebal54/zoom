716f7e0154041ee4336a3093648984f5ff05ff9f
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 20:58:44 +0530

    Initial commit: Add raw to bronze layer transformation models

--- models/bronze/bz_billing_events.sql
+++ models/bronze/bz_billing_events.sql
@@ -1,12 +1,9 @@
-{{  
-    config(
-        materialized='table',
-        pre_hook="{{ pre_hook_log('billing_events') }}",
-        post_hook="{{ post_hook_log('billing_events') }}"
-    )
-}}
+{{config(
+    materialized='table',
+    pre_hook="{% if model.name != 'bz_audit_log' %} {{ log_model_start(model.name) }} {% endif %}",
+    post_hook="{% if model.name != 'bz_audit_log' %} {{ log_model_completion(model.name) }} {% endif %}"
+)}}
 
--- Extract and transform billing events data from raw to bronze
 SELECT
     event_id,
     user_id,
@@ -16,4 +13,4 @@ SELECT
     CURRENT_TIMESTAMP() as load_timestamp,
     CURRENT_TIMESTAMP() as update_timestamp,
     'ZOOM_PLATFORM' as source_system
-FROM {{ source('zoom', 'billing_events') }}
+FROM {{ source('raw', 'billing_events') }}
