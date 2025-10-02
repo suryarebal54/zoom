1f24b6432f6ad9687810219dcf152ed04dacbd21
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 22:01:04 +0530

    Initial commit: Add raw-to-bronze transformation models

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -1,28 +1,18 @@
 {{config(
-    materialized='incremental',
-    unique_key='record_id'
+  materialized = 'incremental',
+  unique_key = 'record_id'
 )}}
 
--- Create a table to track the execution of bronze models
-WITH source_data AS (
-    SELECT
-        {{ dbt_utils.surrogate_key(['source_table', 'load_timestamp', 'status']) }} as record_id,
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-    FROM {{ this }}
-    
-    {% if is_incremental() %}
-        -- Only get new records if this is an incremental run
-        WHERE load_timestamp > (SELECT MAX(load_timestamp) FROM {{ this }})
-    {% endif %}
-)
+-- Create the audit log table if it doesn't exist
+SELECT
+  ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP()) as record_id,
+  'INITIAL' as source_table,
+  CURRENT_TIMESTAMP() as load_timestamp,
+  'SYSTEM' as processed_by,
+  0 as processing_time,
+  'INITIALIZED' as status
 
-SELECT * FROM source_data
-
--- Initial run will have no data, so we need to ensure we have a valid structure
-{% if not is_incremental() %}
-WHERE 1=0
+{% if is_incremental() %}
+  -- This will only be executed on subsequent runs
+  WHERE FALSE
 {% endif %}
