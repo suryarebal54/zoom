ff226a8c5a36c78d5653eca7e36ed2f23c4e3c36
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 23:28:33 +0530

    Initial commit: DBT project for raw to bronze transformation

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,19 +1,18 @@
 {{config(
-  materialized = 'table',
-  schema = 'bronze'
+    materialized='table',
+    schema='bronze',
+    pre_hook="{{ log_audit_start('webinars') }}",
+    post_hook="{{ log_audit_end('webinars') }}"
 )}}
 
--- Extract webinars data from raw source and apply transformations
 SELECT
-  -- Direct mappings from source
-  webinar_id,
-  host_id,
-  webinar_topic,
-  start_time,
-  end_time,
-  registrants,
-  -- Metadata columns
-  CURRENT_TIMESTAMP() as load_timestamp,
-  CURRENT_TIMESTAMP() as update_timestamp,
-  'ZOOM_PLATFORM' as source_system
+    webinar_id,
+    host_id,
+    webinar_topic,
+    start_time,
+    end_time,
+    registrants,
+    load_timestamp,
+    CURRENT_TIMESTAMP() as update_timestamp,
+    'ZOOM_PLATFORM' as source_system
 FROM {{ source('raw', 'webinars') }}
