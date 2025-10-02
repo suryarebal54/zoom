e369131b28d2e34a3592e3ff9c92e3f199437e3e
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 23:30:12 +0530

    Updated DBT code: Fixed audit log references and improved model configuration

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,13 +1,13 @@
 {% macro log_audit_start(source_table) %}
   {% if this.name != 'bz_audit_log' %}
-    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    insert into {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status)
     values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', 0, 'STARTED');
   {% endif %}
 {% endmacro %}
 
 {% macro log_audit_end(source_table) %}
   {% if this.name != 'bz_audit_log' %}
-    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', datediff('second', (select max(load_timestamp) from {{ ref('bz_audit_log') }} where source_table = '{{ source_table }}' and status = 'STARTED'), current_timestamp()), 'COMPLETED');
+    insert into {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status)
+    values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', datediff('second', (select max(load_timestamp) from {{ target.schema }}.bz_audit_log where source_table = '{{ source_table }}' and status = 'STARTED'), current_timestamp()), 'COMPLETED');
   {% endif %}
 {% endmacro %}
