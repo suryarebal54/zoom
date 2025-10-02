7b84a0b60d5cd46f641da9d30f53b0ef155902fd
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 20:58:42 +0530

    Initial commit: Add raw to bronze layer transformation models

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,12 +1,9 @@
-{{  
-    config(
-        materialized='table',
-        pre_hook="{{ pre_hook_log('webinars') }}",
-        post_hook="{{ post_hook_log('webinars') }}"
-    )
-}}
+{{config(
+    materialized='table',
+    pre_hook="{% if model.name != 'bz_audit_log' %} {{ log_model_start(model.name) }} {% endif %}",
+    post_hook="{% if model.name != 'bz_audit_log' %} {{ log_model_completion(model.name) }} {% endif %}"
+)}}
 
--- Extract and transform webinars data from raw to bronze
 SELECT
     webinar_id,
     host_id,
@@ -17,4 +14,4 @@ SELECT
     CURRENT_TIMESTAMP() as load_timestamp,
     CURRENT_TIMESTAMP() as update_timestamp,
     'ZOOM_PLATFORM' as source_system
-FROM {{ source('zoom', 'webinars') }}
+FROM {{ source('raw', 'webinars') }}
