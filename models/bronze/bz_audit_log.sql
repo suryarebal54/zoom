e3b6c4fe34cd1e40e6834100bbe0982b8d523b38
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 23:30:13 +0530

    Updated DBT code: Fixed audit log references and improved model configuration

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -1,9 +1,10 @@
 {{config(
     materialized='table',
-    schema='bronze'
+    schema='bronze',
+    tags=['audit']
 )}}
 
--- Create the audit log table if it doesn't exist
+-- Create the audit log table
 SELECT
     NULL as record_id,
     'INITIAL_SETUP' as source_table,
@@ -11,4 +12,3 @@ SELECT
     '{{ target.user }}' as processed_by,
     0 as processing_time,
     'SETUP' as status
-WHERE NOT EXISTS (SELECT 1 FROM {{ this }})
