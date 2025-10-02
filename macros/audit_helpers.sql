4e3219c5b7fd1bd9e49c254122d163fa09bb5cde
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 21:49:36 +0530

    Add audit_helpers.sql macros

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,48 +1,18 @@
 {% macro log_model_start(model_name) %}
-  {% if execute %}
-    {% set audit_query %}
-      INSERT INTO {{ target.schema }}.bz_audit_log (
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-      )
-      SELECT
-        '{{ model_name }}',
-        CURRENT_TIMESTAMP(),
-        '{{ target.user }}',
-        NULL,
-        'STARTED'
-      ;
-    {% endset %}
-    {% do run_query(audit_query) %}
-  {% endif %}
+    {% if model_name != 'bz_audit_log' %}
+        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+        VALUES ('{{ model_name }}', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'STARTED')
+    {% endif %}
 {% endmacro %}
 
-{% macro log_model_completion(model_name) %}
-  {% if execute %}
-    {% set audit_query %}
-      INSERT INTO {{ target.schema }}.bz_audit_log (
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-      )
-      SELECT
-        '{{ model_name }}',
-        CURRENT_TIMESTAMP(),
-        '{{ target.user }}',
-        DATEDIFF('MILLISECOND', (
-          SELECT MAX(load_timestamp)
-          FROM {{ target.schema }}.bz_audit_log
-          WHERE source_table = '{{ model_name }}'
-          AND status = 'STARTED'
-        ), CURRENT_TIMESTAMP()),
-        'COMPLETED'
-      ;
-    {% endset %}
-    {% do run_query(audit_query) %}
-  {% endif %}
+{% macro log_model_end(model_name) %}
+    {% if model_name != 'bz_audit_log' %}
+        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+        VALUES ('{{ model_name }}', CURRENT_TIMESTAMP(), CURRENT_USER(), 
+                DATEDIFF('SECOND', 
+                         (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} 
+                          WHERE source_table = '{{ model_name }}' AND status = 'STARTED'), 
+                         CURRENT_TIMESTAMP()), 
+                'COMPLETED')
+    {% endif %}
 {% endmacro %}
