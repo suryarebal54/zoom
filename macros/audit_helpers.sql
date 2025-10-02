5d5800ef4455cfb58e384ab1b8513fa7b47e0e72
suryarebal54 <suryarebal54@gmail.com>
2025-10-02 23:28:29 +0530

    Initial commit: DBT project for raw to bronze transformation

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,18 +1,13 @@
-{% macro log_model_start(model_name) %}
-    {% if model_name != 'bz_audit_log' %}
-        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-        VALUES ('{{ model_name }}', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'STARTED')
-    {% endif %}
+{% macro log_audit_start(source_table) %}
+  {% if this.name != 'bz_audit_log' %}
+    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', 0, 'STARTED');
+  {% endif %}
 {% endmacro %}
 
-{% macro log_model_end(model_name) %}
-    {% if model_name != 'bz_audit_log' %}
-        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-        VALUES ('{{ model_name }}', CURRENT_TIMESTAMP(), CURRENT_USER(), 
-                DATEDIFF('SECOND', 
-                         (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} 
-                          WHERE source_table = '{{ model_name }}' AND status = 'STARTED'), 
-                         CURRENT_TIMESTAMP()), 
-                'COMPLETED')
-    {% endif %}
+{% macro log_audit_end(source_table) %}
+  {% if this.name != 'bz_audit_log' %}
+    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', datediff('second', (select max(load_timestamp) from {{ ref('bz_audit_log') }} where source_table = '{{ source_table }}' and status = 'STARTED'), current_timestamp()), 'COMPLETED');
+  {% endif %}
 {% endmacro %}
