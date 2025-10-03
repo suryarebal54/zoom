1e848cfbbd080eb91e1f9cba28f8a501586d09d7
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 13:37:03 +0530

    Add bronze models for feature_usage, webinars, and support_tickets

--- models/bronze/bz_feature_usage.sql
+++ models/bronze/bz_feature_usage.sql
@@ -1,30 +1,19 @@
-{{config(
-  materialized = 'table',
-  pre_hook="{{ log_audit_start('feature_usage') }}",
-  post_hook="{{ log_audit_end('feature_usage') }}"
-)}}
+{{
+  config(
+    materialized = 'table',
+    pre_hook="{{ log_model_start('bz_feature_usage') }}",
+    post_hook="{{ log_model_end('bz_feature_usage') }}"
+  )
+}}
 
-WITH source_data AS (
-  SELECT
-    Usage_ID,
-    Meeting_ID,
-    Feature_Name,
-    Usage_Count,
-    Usage_Date
-  FROM {{ source('zoom', 'feature_usage') }}
-),
-
-final AS (
-  SELECT
-    Usage_ID as usage_id,
-    Meeting_ID as meeting_id,
-    Feature_Name as feature_name,
-    Usage_Count as usage_count,
-    Usage_Date as usage_date,
-    CURRENT_TIMESTAMP() as load_timestamp,
-    CURRENT_TIMESTAMP() as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
-  FROM source_data
-)
-
-SELECT * FROM final
+-- Extract and transform feature usage data from raw to bronze
+select
+  usage_id,
+  meeting_id,
+  feature_name,
+  usage_count,
+  usage_date,
+  current_timestamp() as load_timestamp,
+  current_timestamp() as update_timestamp,
+  'ZOOM_PLATFORM' as source_system
+from {{ source('raw', 'feature_usage') }}
