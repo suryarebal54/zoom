f4a2d651c0b1eed556cd1ce9f72361e661b18984
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 17:36:55 +0530

    Initial commit for raw to bronze layer transformation

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,21 +1,17 @@
 {{config(
-    materialized='table',
-    pre_hook="{{ log_audit_start('webinars') }}",
-    post_hook="{{ log_audit_end('webinars') }}"
+  materialized = 'table',
+  pre_hook="{% if this.identifier != 'bz_audit_log' %} {{ log_table_load('webinars', 'STARTED') }} {% endif %}",
+  post_hook="{% if this.identifier != 'bz_audit_log' %} {{ log_table_load('webinars', 'COMPLETED') }} {% endif %}"
 )}}
 
--- Extract and transform webinars data from source to bronze layer
-select
-    -- Business data fields
-    webinar_id,
-    host_id,
-    webinar_topic,
-    start_time,
-    end_time,
-    registrants,
-    
-    -- Metadata fields
-    current_timestamp() as load_timestamp,
-    current_timestamp() as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
-from {{ source('zoom', 'webinars') }}
+SELECT
+  webinar_id,
+  host_id,
+  webinar_topic,
+  start_time,
+  end_time,
+  registrants,
+  CURRENT_TIMESTAMP() as load_timestamp,
+  CURRENT_TIMESTAMP() as update_timestamp,
+  '{{ var("source_system") }}' as source_system
+FROM {{ source('raw', 'webinars') }}
