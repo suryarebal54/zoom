981dc9af867a350089e1bf5a70c575ac20981b79
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 15:36:29 +0530

    Initial commit: Add raw to bronze transformation models

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,19 +1,37 @@
 {{config(
     materialized='table',
-    pre_hook="{% set start_time = modules.datetime.datetime.now() %} {{ log_audit_start('webinars') }}",
-    post_hook="{{ log_audit_end('webinars', 'timestamp\'' + start_time.strftime('%Y-%m-%d %H:%M:%S') + '\'') }}"
+    schema='bronze'
 )}}
 
-SELECT
-    -- Source columns
-    Webinar_ID as webinar_id,
-    Host_ID as host_id,
-    Webinar_Topic as webinar_topic,
-    Start_Time as start_time,
-    End_Time as end_time,
-    Registrants as registrants,
-    -- Metadata columns
-    CURRENT_TIMESTAMP() as load_timestamp,
-    CURRENT_TIMESTAMP() as update_timestamp,
-    '{{ var("source_system") }}' as source_system
-FROM {{ source('raw', 'webinars') }}
+WITH source_data AS (
+    SELECT
+        webinar_id,
+        host_id,
+        webinar_topic,
+        start_time,
+        end_time,
+        registrants,
+        load_timestamp,
+        update_timestamp,
+        source_system
+    FROM {{ source('raw', 'webinars') }}
+),
+
+validated_data AS (
+    SELECT
+        -- Primary fields
+        COALESCE(webinar_id, 'UNKNOWN') AS webinar_id,
+        host_id,
+        webinar_topic,
+        start_time,
+        end_time,
+        registrants,
+        
+        -- Metadata fields
+        COALESCE(load_timestamp, CURRENT_TIMESTAMP()) AS load_timestamp,
+        COALESCE(update_timestamp, CURRENT_TIMESTAMP()) AS update_timestamp,
+        COALESCE(source_system, 'ZOOM_PLATFORM') AS source_system
+    FROM source_data
+)
+
+SELECT * FROM validated_data
