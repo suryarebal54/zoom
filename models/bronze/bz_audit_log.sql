82fa13633fb50ca039c9bc9c027170cd25353712
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 15:36:26 +0530

    Initial commit: Add raw to bronze transformation models

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -4,11 +4,34 @@
 )}}
 
 -- Create the audit log table if it doesn't exist
-SELECT
-    NULL as record_id,
-    'INITIAL_SETUP' as source_table,
-    CURRENT_TIMESTAMP() as load_timestamp,
-    '{{ target.user }}' as processed_by,
-    0 as processing_time,
-    'SETUP' as status
-WHERE NOT EXISTS (SELECT 1 FROM {{ this }})
+CREATE TABLE IF NOT EXISTS {{ this }} (
+    record_id NUMBER AUTOINCREMENT,
+    source_table STRING,
+    load_timestamp TIMESTAMP_NTZ,
+    processed_by STRING,
+    processing_time NUMBER,
+    status STRING
+);
+
+-- For incremental runs, we don't need to select anything
+{% if is_incremental() %}
+    SELECT
+        record_id,
+        source_table,
+        load_timestamp,
+        processed_by,
+        processing_time,
+        status
+    FROM {{ this }}
+    WHERE 1=0
+{% else %}
+    -- For the initial run, create an empty result set
+    SELECT
+        CAST(NULL AS NUMBER) AS record_id,
+        CAST(NULL AS STRING) AS source_table,
+        CAST(NULL AS TIMESTAMP_NTZ) AS load_timestamp,
+        CAST(NULL AS STRING) AS processed_by,
+        CAST(NULL AS NUMBER) AS processing_time,
+        CAST(NULL AS STRING) AS status
+    WHERE 1=0
+{% endif %}
