53778698d9b33fd3c80690d6de0fc1e44cc4237d
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 13:36:51 +0530

    Add bronze models for users, meetings, and participants

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -1,32 +1,20 @@
-{{config(
-  materialized = 'table',
-  pre_hook="{{ log_audit_start('meetings') }}",
-  post_hook="{{ log_audit_end('meetings') }}"
-)}}
+{{
+  config(
+    materialized = 'table',
+    pre_hook="{{ log_model_start('bz_meetings') }}",
+    post_hook="{{ log_model_end('bz_meetings') }}"
+  )
+}}
 
-WITH source_data AS (
-  SELECT
-    Meeting_ID,
-    Host_ID,
-    Meeting_Topic,
-    Start_Time,
-    End_Time,
-    Duration_Minutes
-  FROM {{ source('zoom', 'meetings') }}
-),
-
-final AS (
-  SELECT
-    Meeting_ID as meeting_id,
-    Host_ID as host_id,
-    Meeting_Topic as meeting_topic,
-    Start_Time as start_time,
-    End_Time as end_time,
-    Duration_Minutes as duration_minutes,
-    CURRENT_TIMESTAMP() as load_timestamp,
-    CURRENT_TIMESTAMP() as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
-  FROM source_data
-)
-
-SELECT * FROM final
+-- Extract and transform meetings data from raw to bronze
+select
+  meeting_id,
+  host_id,
+  meeting_topic,
+  start_time,
+  end_time,
+  duration_minutes,
+  current_timestamp() as load_timestamp,
+  current_timestamp() as update_timestamp,
+  'ZOOM_PLATFORM' as source_system
+from {{ source('raw', 'meetings') }}
