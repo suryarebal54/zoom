eeb14c855899a125a3ab7d862acd0c84e8b9ef15
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 13:35:36 +0530

    Add audit helper macros

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,19 +1,41 @@
-{% macro log_audit_start(source_table) %}
-  {% if execute and not this.name == 'bz_audit_log' %}
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-    VALUES ('{{ source_table }}', CURRENT_TIMESTAMP(), '{{ target.user }}', 'STARTED');
+{% macro log_model_start(model_name) %}
+  {% if model_name != 'bz_audit_log' %}
+    insert into {{ ref('bz_audit_log') }} (
+      source_table, 
+      load_timestamp, 
+      processed_by, 
+      processing_time, 
+      status
+    ) 
+    values (
+      '{{ model_name }}',
+      current_timestamp(),
+      '{{ target.user }}',
+      null,
+      'STARTED'
+    )
   {% endif %}
 {% endmacro %}
 
-{% macro log_audit_end(source_table) %}
-  {% if execute and not this.name == 'bz_audit_log' %}
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    VALUES (
-      '{{ source_table }}', 
-      CURRENT_TIMESTAMP(), 
-      '{{ target.user }}', 
-      DATEDIFF(SECOND, (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = '{{ source_table }}' AND status = 'STARTED'), CURRENT_TIMESTAMP()),
+{% macro log_model_end(model_name) %}
+  {% if model_name != 'bz_audit_log' %}
+    insert into {{ ref('bz_audit_log') }} (
+      source_table, 
+      load_timestamp, 
+      processed_by, 
+      processing_time, 
+      status
+    ) 
+    select 
+      '{{ model_name }}',
+      current_timestamp(),
+      '{{ target.user }}',
+      datediff('millisecond', max(load_timestamp), current_timestamp()) / 1000.0,
       'COMPLETED'
-    );
+    from {{ ref('bz_audit_log') }}
+    where source_table = '{{ model_name }}'
+    and status = 'STARTED'
+    order by load_timestamp desc
+    limit 1
   {% endif %}
 {% endmacro %}
