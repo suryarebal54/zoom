5ea5f8044893dd47ef767fbab7cb68306ab36324
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 16:04:33 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,37 +1,21 @@
 {{config(
     materialized='table',
-    schema='bronze'
+    pre_hook="{{ log_audit_start('webinars') }}",
+    post_hook="{{ log_audit_end('webinars') }}"
 )}}
 
-WITH source_data AS (
-    SELECT
-        webinar_id,
-        host_id,
-        webinar_topic,
-        start_time,
-        end_time,
-        registrants,
-        load_timestamp,
-        update_timestamp,
-        source_system
-    FROM {{ source('raw', 'webinars') }}
-),
-
-validated_data AS (
-    SELECT
-        -- Primary fields
-        COALESCE(webinar_id, 'UNKNOWN') AS webinar_id,
-        host_id,
-        webinar_topic,
-        start_time,
-        end_time,
-        registrants,
-        
-        -- Metadata fields
-        COALESCE(load_timestamp, CURRENT_TIMESTAMP()) AS load_timestamp,
-        COALESCE(update_timestamp, CURRENT_TIMESTAMP()) AS update_timestamp,
-        COALESCE(source_system, 'ZOOM_PLATFORM') AS source_system
-    FROM source_data
-)
-
-SELECT * FROM validated_data
+-- Extract and transform webinars data from source to bronze layer
+select
+    -- Business data fields
+    webinar_id,
+    host_id,
+    webinar_topic,
+    start_time,
+    end_time,
+    registrants,
+    
+    -- Metadata fields
+    current_timestamp() as load_timestamp,
+    current_timestamp() as update_timestamp,
+    'ZOOM_PLATFORM' as source_system
+from {{ source('zoom', 'webinars') }}
