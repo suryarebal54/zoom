fa457dfa00fec2578abf0440a3e12e84e4fda290
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 14:03:33 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -1,34 +1,18 @@
-{{ config(
+{{config(
     materialized='table',
-    schema='bronze'
-) }}
+    pre_hook="{% set start_time = modules.datetime.datetime.now() %} {{ log_audit_start('users') }}",
+    post_hook="{{ log_audit_end('users', 'timestamp\'' + start_time.strftime('%Y-%m-%d %H:%M:%S') + '\'') }}"
+)}}
 
-with source_data as (
-    select
-        user_id,
-        user_name,
-        email,
-        company,
-        plan_type,
-        load_timestamp,
-        update_timestamp,
-        source_system
-    from {{ source('raw', 'users') }}
-),
-
-final as (
-    select
-        -- Direct mappings from source
-        user_id,
-        user_name,
-        email,
-        company,
-        plan_type,
-        -- Metadata columns
-        load_timestamp,
-        current_timestamp() as update_timestamp,
-        coalesce(source_system, 'ZOOM_PLATFORM') as source_system
-    from source_data
-)
-
-select * from final
+SELECT
+    -- Source columns
+    User_ID as user_id,
+    User_Name as user_name,
+    Email as email,
+    Company as company,
+    Plan_Type as plan_type,
+    -- Metadata columns
+    CURRENT_TIMESTAMP() as load_timestamp,
+    CURRENT_TIMESTAMP() as update_timestamp,
+    '{{ var("source_system") }}' as source_system
+FROM {{ source('raw', 'users') }}
