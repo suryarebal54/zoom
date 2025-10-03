05a2e73b09aad53e378969bd1acb551f1813d5b9
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 14:03:32 +0530

    Initial commit: Add bronze layer transformation models

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,21 +1,19 @@
-{% macro log_model_start() %}
-  {% if this.name != 'bz_audit_log' %}
-    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    values ('{{ this.name }}', current_timestamp(), '{{ target.user }}', null, 'STARTED');
-  {% endif %}
+{% macro log_audit_start(source_table) %}
+    {% if this.name != 'bz_audit_log' %}
+        insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
+        values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', 'STARTED')
+    {% endif %}
 {% endmacro %}
 
-{% macro log_model_end() %}
-  {% if this.name != 'bz_audit_log' %}
-    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    values ('{{ this.name }}', current_timestamp(), '{{ target.user }}', datediff('millisecond', (select max(load_timestamp) from {{ ref('bz_audit_log') }} where source_table = '{{ this.name }}' and status = 'STARTED'), current_timestamp()), 'COMPLETED');
-  {% endif %}
-{% endmacro %}
-
-{% macro generate_schema_name(custom_schema_name, node) %}
-  {% if custom_schema_name is none %}
-    {{ return(target.schema) }}
-  {% else %}
-    {{ return(custom_schema_name) }}
-  {% endif %}
+{% macro log_audit_end(source_table, start_time) %}
+    {% if this.name != 'bz_audit_log' %}
+        insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+        values (
+            '{{ source_table }}',
+            current_timestamp(),
+            '{{ target.user }}',
+            datediff('second', {{ start_time }}, current_timestamp()),
+            'COMPLETED'
+        )
+    {% endif %}
 {% endmacro %}
