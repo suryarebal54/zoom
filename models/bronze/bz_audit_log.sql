0ed66f4990647170c8f3b6d61785f3f31182e94b
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 14:03:32 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -1,19 +1,14 @@
-{{ config(
+{{config(
     materialized='incremental',
-    unique_key='record_id',
-    schema='bronze'
-) }}
+    unique_key='record_id'
+)}}
 
 -- Create the audit log table if it doesn't exist
-{% if is_incremental() %}
-    select * from {{ this }} where 1=0
-{% else %}
-    select
-        null as record_id,
-        'INITIAL_SETUP' as source_table,
-        current_timestamp() as load_timestamp,
-        '{{ target.user }}' as processed_by,
-        0 as processing_time,
-        'COMPLETED' as status
-    where 1=0
-{% endif %}
+SELECT
+    NULL as record_id,
+    'INITIAL_SETUP' as source_table,
+    CURRENT_TIMESTAMP() as load_timestamp,
+    '{{ target.user }}' as processed_by,
+    0 as processing_time,
+    'SETUP' as status
+WHERE NOT EXISTS (SELECT 1 FROM {{ this }})
