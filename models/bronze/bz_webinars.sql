71399d64c6f1071158a6e417ad061e292f41f551
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 13:37:04 +0530

    Add bronze models for feature_usage, webinars, and support_tickets

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,32 +1,20 @@
-{{config(
-  materialized = 'table',
-  pre_hook="{{ log_audit_start('webinars') }}",
-  post_hook="{{ log_audit_end('webinars') }}"
-)}}
+{{
+  config(
+    materialized = 'table',
+    pre_hook="{{ log_model_start('bz_webinars') }}",
+    post_hook="{{ log_model_end('bz_webinars') }}"
+  )
+}}
 
-WITH source_data AS (
-  SELECT
-    Webinar_ID,
-    Host_ID,
-    Webinar_Topic,
-    Start_Time,
-    End_Time,
-    Registrants
-  FROM {{ source('zoom', 'webinars') }}
-),
-
-final AS (
-  SELECT
-    Webinar_ID as webinar_id,
-    Host_ID as host_id,
-    Webinar_Topic as webinar_topic,
-    Start_Time as start_time,
-    End_Time as end_time,
-    Registrants as registrants,
-    CURRENT_TIMESTAMP() as load_timestamp,
-    CURRENT_TIMESTAMP() as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
-  FROM source_data
-)
-
-SELECT * FROM final
+-- Extract and transform webinars data from raw to bronze
+select
+  webinar_id,
+  host_id,
+  webinar_topic,
+  start_time,
+  end_time,
+  registrants,
+  current_timestamp() as load_timestamp,
+  current_timestamp() as update_timestamp,
+  'ZOOM_PLATFORM' as source_system
+from {{ source('raw', 'webinars') }}
