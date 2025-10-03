063d4213aa1984873f7fc025753a5f28b1118a49
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 17:36:51 +0530

    Initial commit for raw to bronze layer transformation

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,20 +1,20 @@
-{% macro log_audit_start(source_table) %}
-    {% if this.name != 'bz_audit_log' %}
-        insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-        values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', 0, 'STARTED');
-    {% endif %}
-{% endmacro %}
-
-{% macro log_audit_end(source_table) %}
-    {% if this.name != 'bz_audit_log' %}
-        insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-        select 
-            '{{ source_table }}',
-            current_timestamp(),
-            '{{ target.user }}',
-            datediff('second', max(load_timestamp), current_timestamp()),
-            'COMPLETED'
-        from {{ ref('bz_audit_log') }}
-        where source_table = '{{ source_table }}' and status = 'STARTED';
-    {% endif %}
+{% macro log_table_load(source_table, status) %}
+  {% if execute %}
+    {% set query %}
+      INSERT INTO {{ ref('bz_audit_log') }} (
+        source_table,
+        load_timestamp,
+        processed_by,
+        processing_time,
+        status
+      )
+      SELECT
+        '{{ source_table }}',
+        CURRENT_TIMESTAMP(),
+        CURRENT_USER(),
+        DATEDIFF('MILLISECOND', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()) / 1000.0,
+        '{{ status }}'
+    {% endset %}
+    {% do run_query(query) %}
+  {% endif %}
 {% endmacro %}
