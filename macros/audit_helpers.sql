9863e902a1266b3141a51fcc572c88af81bac4ce
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 12:02:54 +0530

    Initial commit: Add bronze layer models for Zoom Analytics

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,68 +1,19 @@
-{% macro audit_start_hook() %}
-  {% if this.name != 'bz_audit_log' %}
-    {% set query %}
-      INSERT INTO {{ ref('bz_audit_log') }} (
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-      )
-      SELECT
-        '{{ this.name }}',
-        CURRENT_TIMESTAMP(),
-        CURRENT_USER(),
-        NULL,
-        'STARTED'
-      {% endset %}
-      {% do run_query(query) %}
+{% macro log_audit_start(source_table) %}
+  {% if execute and not this.name == 'bz_audit_log' %}
+    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
+    VALUES ('{{ source_table }}', CURRENT_TIMESTAMP(), '{{ target.user }}', 'STARTED');
   {% endif %}
 {% endmacro %}
 
-{% macro audit_end_hook() %}
-  {% if this.name != 'bz_audit_log' %}
-    {% set query %}
-      INSERT INTO {{ ref('bz_audit_log') }} (
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-      )
-      SELECT
-        '{{ this.name }}',
-        CURRENT_TIMESTAMP(),
-        CURRENT_USER(),
-        DATEDIFF('SECOND', (
-          SELECT MAX(load_timestamp)
-          FROM {{ ref('bz_audit_log') }}
-          WHERE source_table = '{{ this.name }}'
-          AND status = 'STARTED'
-        ), CURRENT_TIMESTAMP()),
-        'COMPLETED'
-      {% endset %}
-      {% do run_query(query) %}
-  {% endif %}
-{% endmacro %}
-
-{% macro error_handler() %}
-  {% if execute %}
-    {% do exceptions.warn("Error occurred during model execution: " ~ this.name) %}
-    {% set query %}
-      INSERT INTO {{ ref('bz_audit_log') }} (
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-      )
-      SELECT
-        '{{ this.name }}',
-        CURRENT_TIMESTAMP(),
-        CURRENT_USER(),
-        NULL,
-        'FAILED'
-      {% endset %}
-      {% do run_query(query) %}
+{% macro log_audit_end(source_table) %}
+  {% if execute and not this.name == 'bz_audit_log' %}
+    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    VALUES (
+      '{{ source_table }}', 
+      CURRENT_TIMESTAMP(), 
+      '{{ target.user }}', 
+      DATEDIFF(SECOND, (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = '{{ source_table }}' AND status = 'STARTED'), CURRENT_TIMESTAMP()),
+      'COMPLETED'
+    );
   {% endif %}
 {% endmacro %}
