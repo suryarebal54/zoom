0a46ef5143bb8f8249d272de6d455df2fc4b0bdf
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 16:04:34 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_licenses.sql
+++ models/bronze/bz_licenses.sql
@@ -1,35 +1,20 @@
 {{config(
     materialized='table',
-    schema='bronze'
+    pre_hook="{{ log_audit_start('licenses') }}",
+    post_hook="{{ log_audit_end('licenses') }}"
 )}}
 
-WITH source_data AS (
-    SELECT
-        license_id,
-        license_type,
-        assigned_to_user_id,
-        start_date,
-        end_date,
-        load_timestamp,
-        update_timestamp,
-        source_system
-    FROM {{ source('raw', 'licenses') }}
-),
-
-validated_data AS (
-    SELECT
-        -- Primary fields
-        COALESCE(license_id, 'UNKNOWN') AS license_id,
-        license_type,
-        assigned_to_user_id,
-        start_date,
-        end_date,
-        
-        -- Metadata fields
-        COALESCE(load_timestamp, CURRENT_TIMESTAMP()) AS load_timestamp,
-        COALESCE(update_timestamp, CURRENT_TIMESTAMP()) AS update_timestamp,
-        COALESCE(source_system, 'ZOOM_PLATFORM') AS source_system
-    FROM source_data
-)
-
-SELECT * FROM validated_data
+-- Extract and transform licenses data from source to bronze layer
+select
+    -- Business data fields
+    license_id,
+    license_type,
+    assigned_to_user_id,
+    start_date,
+    end_date,
+    
+    -- Metadata fields
+    current_timestamp() as load_timestamp,
+    current_timestamp() as update_timestamp,
+    'ZOOM_PLATFORM' as source_system
+from {{ source('zoom', 'licenses') }}
