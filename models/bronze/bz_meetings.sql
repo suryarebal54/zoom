a43fff1ef4db61806a3c9bbc2b4d44eea17792cf
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 16:04:31 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -1,37 +1,21 @@
 {{config(
     materialized='table',
-    schema='bronze'
+    pre_hook="{{ log_audit_start('meetings') }}",
+    post_hook="{{ log_audit_end('meetings') }}"
 )}}
 
-WITH source_data AS (
-    SELECT
-        meeting_id,
-        host_id,
-        meeting_topic,
-        start_time,
-        end_time,
-        duration_minutes,
-        load_timestamp,
-        update_timestamp,
-        source_system
-    FROM {{ source('raw', 'meetings') }}
-),
-
-validated_data AS (
-    SELECT
-        -- Primary fields
-        COALESCE(meeting_id, 'UNKNOWN') AS meeting_id,
-        host_id,
-        meeting_topic,
-        start_time,
-        end_time,
-        duration_minutes,
-        
-        -- Metadata fields
-        COALESCE(load_timestamp, CURRENT_TIMESTAMP()) AS load_timestamp,
-        COALESCE(update_timestamp, CURRENT_TIMESTAMP()) AS update_timestamp,
-        COALESCE(source_system, 'ZOOM_PLATFORM') AS source_system
-    FROM source_data
-)
-
-SELECT * FROM validated_data
+-- Extract and transform meetings data from source to bronze layer
+select
+    -- Business data fields
+    meeting_id,
+    host_id,
+    meeting_topic,
+    start_time,
+    end_time,
+    duration_minutes,
+    
+    -- Metadata fields
+    current_timestamp() as load_timestamp,
+    current_timestamp() as update_timestamp,
+    'ZOOM_PLATFORM' as source_system
+from {{ source('zoom', 'meetings') }}
