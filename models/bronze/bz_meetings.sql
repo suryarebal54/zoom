8637862e4dd54a09f57ac58df77e8000cd5ef249
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 14:03:33 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -1,36 +1,19 @@
-{{ config(
+{{config(
     materialized='table',
-    schema='bronze'
-) }}
+    pre_hook="{% set start_time = modules.datetime.datetime.now() %} {{ log_audit_start('meetings') }}",
+    post_hook="{{ log_audit_end('meetings', 'timestamp\'' + start_time.strftime('%Y-%m-%d %H:%M:%S') + '\'') }}"
+)}}
 
-with source_data as (
-    select
-        meeting_id,
-        host_id,
-        meeting_topic,
-        start_time,
-        end_time,
-        duration_minutes,
-        load_timestamp,
-        update_timestamp,
-        source_system
-    from {{ source('raw', 'meetings') }}
-),
-
-final as (
-    select
-        -- Direct mappings from source
-        meeting_id,
-        host_id,
-        meeting_topic,
-        start_time,
-        end_time,
-        duration_minutes,
-        -- Metadata columns
-        load_timestamp,
-        current_timestamp() as update_timestamp,
-        coalesce(source_system, 'ZOOM_PLATFORM') as source_system
-    from source_data
-)
-
-select * from final
+SELECT
+    -- Source columns
+    Meeting_ID as meeting_id,
+    Host_ID as host_id,
+    Meeting_Topic as meeting_topic,
+    Start_Time as start_time,
+    End_Time as end_time,
+    Duration_Minutes as duration_minutes,
+    -- Metadata columns
+    CURRENT_TIMESTAMP() as load_timestamp,
+    CURRENT_TIMESTAMP() as update_timestamp,
+    '{{ var("source_system") }}' as source_system
+FROM {{ source('raw', 'meetings') }}
