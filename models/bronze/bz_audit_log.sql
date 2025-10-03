646b221bf5f55917f4bb2a1a334cd72e18b833cd
suryarebal54 <suryarebal54@gmail.com>
2025-10-03 16:04:30 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -3,35 +3,19 @@
     unique_key='record_id'
 )}}
 
--- Create the audit log table if it doesn't exist
-CREATE TABLE IF NOT EXISTS {{ this }} (
-    record_id NUMBER AUTOINCREMENT,
-    source_table STRING,
-    load_timestamp TIMESTAMP_NTZ,
-    processed_by STRING,
-    processing_time NUMBER,
-    status STRING
-);
+-- Initialize the audit log table if it doesn't exist
+with audit_log as (
+    {% if is_incremental() %}
+        select * from {{ this }}
+    {% else %}
+        select
+            1 as record_id,
+            'INITIALIZATION' as source_table,
+            current_timestamp() as load_timestamp,
+            '{{ target.user }}' as processed_by,
+            0 as processing_time,
+            'COMPLETED' as status
+    {% endif %}
+)
 
--- For incremental runs, we don't need to select anything
-{% if is_incremental() %}
-    SELECT
-        record_id,
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-    FROM {{ this }}
-    WHERE 1=0
-{% else %}
-    -- For the initial run, create an empty result set
-    SELECT
-        CAST(NULL AS NUMBER) AS record_id,
-        CAST(NULL AS STRING) AS source_table,
-        CAST(NULL AS TIMESTAMP_NTZ) AS load_timestamp,
-        CAST(NULL AS STRING) AS processed_by,
-        CAST(NULL AS NUMBER) AS processing_time,
-        CAST(NULL AS STRING) AS status
-    WHERE 1=0
-{% endif %}
+select * from audit_log
