04c34a5c30facf39fb3b33ee1ff609a04cb35fd0
suryarebal54 <suryarebal54@gmail.com>
2025-10-06 12:45:43 +0530

    Initial commit: Add bronze layer models for raw to bronze transformation

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -1,14 +1,15 @@
-{{config(
+{{ config(
   materialized = 'incremental',
   unique_key = 'record_id'
-)}}
+) }}
 
--- Create audit log table if it doesn't exist
+-- Create the audit log table if it doesn't exist
 SELECT
-  ROW_NUMBER() OVER (ORDER BY CURRENT_TIMESTAMP()) as record_id,
-  'INITIAL_SETUP' as source_table,
-  CURRENT_TIMESTAMP() as load_timestamp,
-  CURRENT_USER() as processed_by,
-  0 as processing_time,
-  'SUCCESS' as status
-WHERE NOT EXISTS (SELECT 1 FROM {{ this }})
+  record_id,
+  source_table,
+  load_timestamp,
+  processed_by,
+  processing_time,
+  status
+FROM {{ this }}
+WHERE 1=0
