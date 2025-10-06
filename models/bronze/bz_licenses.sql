b9bf218604478cb42678af1df1b78df4c8edf7a3
suryarebal54 <suryarebal54@gmail.com>
2025-10-06 13:17:03 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_licenses.sql
+++ models/bronze/bz_licenses.sql
@@ -1,43 +1,29 @@
 {{config(
-  materialized = 'table',
-  schema = 'bronze'
+    materialized='table',
+    pre_hook="{{ log_audit_start('licenses') }}",
+    post_hook="{{ log_audit_end('licenses') }}"
 )}}
 
-WITH source_data AS (
-  SELECT
-    license_id,
-    license_type,
-    assigned_to_user_id,
-    start_date,
-    end_date,
-    load_timestamp,
-    update_timestamp,
-    source_system
-  FROM {{ source('raw', 'licenses') }}
-),
-
-validated_data AS (
-  SELECT
-    -- Primary fields
-    license_id,
-    license_type,
-    assigned_to_user_id,
-    start_date,
-    end_date,
-    -- Metadata fields
-    load_timestamp,
-    update_timestamp,
-    source_system
-  FROM source_data
+with source_data as (
+    select
+        license_id,
+        license_type,
+        assigned_to_user_id,
+        start_date,
+        end_date,
+        load_timestamp,
+        update_timestamp,
+        source_system
+    from {{ source('raw', 'licenses') }}
 )
 
-SELECT
-  license_id,
-  license_type,
-  assigned_to_user_id,
-  start_date,
-  end_date,
-  COALESCE(load_timestamp, CURRENT_TIMESTAMP()) AS load_timestamp,
-  COALESCE(update_timestamp, CURRENT_TIMESTAMP()) AS update_timestamp,
-  COALESCE(source_system, 'ZOOM_PLATFORM') AS source_system
-FROM validated_data
+select
+    license_id,
+    license_type,
+    assigned_to_user_id,
+    start_date,
+    end_date,
+    coalesce(load_timestamp, current_timestamp()) as load_timestamp,
+    coalesce(update_timestamp, current_timestamp()) as update_timestamp,
+    coalesce(source_system, 'ZOOM_PLATFORM') as source_system
+from source_data
