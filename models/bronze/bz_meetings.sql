c9bfcf70803696cff9d308ff4584ac69abc293ec
suryarebal54 <suryarebal54@gmail.com>
2025-10-06 14:46:31 +0530

    Initial commit: Add bronze layer transformation models

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -1,31 +1,17 @@
 {{config(
-    materialized='table',
-    pre_hook="{{ log_audit_start('meetings') }}",
-    post_hook="{{ log_audit_end('meetings') }}"
+  materialized = 'table',
+  pre_hook="{{ log_audit_start('meetings') }}",
+  post_hook="{{ log_audit_end('meetings') }}"
 )}}
 
-with source_data as (
-    select
-        meeting_id,
-        host_id,
-        meeting_topic,
-        start_time,
-        end_time,
-        duration_minutes,
-        load_timestamp,
-        update_timestamp,
-        source_system
-    from {{ source('raw', 'meetings') }}
-)
-
 select
-    meeting_id,
-    host_id,
-    meeting_topic,
-    start_time,
-    end_time,
-    duration_minutes,
-    coalesce(load_timestamp, current_timestamp()) as load_timestamp,
-    coalesce(update_timestamp, current_timestamp()) as update_timestamp,
-    coalesce(source_system, 'ZOOM_PLATFORM') as source_system
-from source_data
+  meeting_id,
+  host_id,
+  meeting_topic,
+  start_time,
+  end_time,
+  duration_minutes,
+  load_timestamp,
+  update_timestamp,
+  source_system
+from {{ source('raw', 'meetings') }}
