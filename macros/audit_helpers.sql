70de5522244dc8004fef9931cff9d3d15f0f95fd
suryarebal54 <suryarebal54@gmail.com>
2025-10-06 12:45:42 +0530

    Initial commit: Add bronze layer models for raw to bronze transformation

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,20 +1,17 @@
-{% macro log_table_load(source_table, status) %}
-  {% if execute %}
-    {% set query %}
-      INSERT INTO {{ ref('bz_audit_log') }} (
-        source_table,
-        load_timestamp,
-        processed_by,
-        processing_time,
-        status
-      )
-      SELECT
-        '{{ source_table }}',
-        CURRENT_TIMESTAMP(),
-        CURRENT_USER(),
-        DATEDIFF('MILLISECOND', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()) / 1000.0,
-        '{{ status }}'
-    {% endset %}
-    {% do run_query(query) %}
+{% macro log_model_start() %}
+  {% if this.name != 'bz_audit_log' %}
+    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    values ('{{ this.name }}', current_timestamp(), '{{ target.user }}', 0, 'STARTED');
   {% endif %}
+{% endmacro %}
+
+{% macro log_model_end() %}
+  {% if this.name != 'bz_audit_log' %}
+    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    values ('{{ this.name }}', current_timestamp(), '{{ target.user }}', datediff('second', (select max(load_timestamp) from {{ ref('bz_audit_log') }} where source_table = '{{ this.name }}' and status = 'STARTED'), current_timestamp()), 'COMPLETED');
+  {% endif %}
+{% endmacro %}
+
+{% macro handle_null_values(column_name, default_value) %}
+  coalesce({{ column_name }}, {{ default_value }})
 {% endmacro %}
