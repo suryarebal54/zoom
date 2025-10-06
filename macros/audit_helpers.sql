6c97cdd22b0b8f71cad075e3d2340b477829f21a
suryarebal54 <suryarebal54@gmail.com>
2025-10-06 14:46:30 +0530

    Initial commit: Add bronze layer transformation models

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,20 +1,20 @@
 {% macro log_audit_start(source_table) %}
   {% if this.name != 'bz_audit_log' %}
-    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-    values ('{{ source_table }}', current_timestamp(), current_user(), 'STARTED');
+    insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    values ('{{ source_table }}', current_timestamp(), '{{ target.user }}', null, 'STARTED')
   {% endif %}
 {% endmacro %}
 
 {% macro log_audit_end(source_table) %}
   {% if this.name != 'bz_audit_log' %}
     insert into {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    select 
+    select
       '{{ source_table }}',
       current_timestamp(),
-      current_user(),
+      '{{ target.user }}',
       datediff('second', max(load_timestamp), current_timestamp()),
       'COMPLETED'
     from {{ ref('bz_audit_log') }}
-    where source_table = '{{ source_table }}' and status = 'STARTED';
+    where source_table = '{{ source_table }}' and status = 'STARTED'
   {% endif %}
 {% endmacro %}
